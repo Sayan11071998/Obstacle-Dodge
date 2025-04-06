b5a96eee885961d7480eeaf9fe1c375ac6b9b477
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-07 04:06:33 +0530

    New Scripts Added

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,15 +6,26 @@ public class Mover : MonoBehaviour
 
     private void Start()
     {
-
+        PrintInstructions();
     }
 
     private void Update()
     {
+        MovePlayer();
+    }
+
+    private void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys or wasd");
+        Debug.Log("Don't bump into objects");
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
         transform.Translate(xValue, yValue, zValue);
     }
 }
