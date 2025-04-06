ee055299f82da6a80037c694d3955feedf9c492c
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-07 01:49:38 +0530

    Input System Created

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 10f;
+
     private void Start()
     {
 
@@ -9,6 +11,10 @@ public class Mover : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
