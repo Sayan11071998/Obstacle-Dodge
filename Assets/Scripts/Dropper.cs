1209a13a4a661da3ae199441aeee0662981a4c10
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-07 12:16:37 +0530

    Dropper Added

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] private float timeToWait = 2f;
+
+    private MeshRenderer myMeshRenderer;
+    private Rigidbody myRigidBody;
+
+    private void Start()
+    {
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myRigidBody = GetComponent<Rigidbody>();
+
+        myMeshRenderer.enabled = false;
+        myRigidBody.useGravity = false;
+    }
+
     private void Update()
     {
-        Debug.Log(Time.time);
+        if (Time.time > timeToWait)
+        {
+            myMeshRenderer.enabled = true;
+            myRigidBody.useGravity = true;
+        }
     }
 }
